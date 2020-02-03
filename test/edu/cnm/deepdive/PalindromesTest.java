00daba6a73317a44dcef6d9834779a5363af31e5
Jose Ortiz <jose.ortiz@live.com>
2020-02-03 11:51:47 -0700

    Display test name.

--- test/edu/cnm/deepdive/PalindromesTest.java
+++ test/edu/cnm/deepdive/PalindromesTest.java
@@ -3,6 +3,7 @@ package edu.cnm.deepdive;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Objects;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
@@ -10,13 +11,13 @@ import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class PalindromesTest {
-
+  @DisplayName("Test known Palindromes")
   @ParameterizedTest
   @CsvFileSource(resources = "/affirmative.csv")
   void isPalindromeAffirmative(String testCase) {
       assertTrue(Palindromes.isPalindrome(testCase));
     }
-
+  @DisplayName("Test known non-Palindromes")
   @ParameterizedTest
   @CsvFileSource( resources = "/negative.csv")
   void isPalindromeNegative(String testCase) {
